b8c5debe891fe039a7284c52013b7bf566ae279e
Antoš Michael <antosmichael07@gmail.com>
2024-06-08 17:06:18 +0200

    i think i fixed the issue

--- go_tcp_connection.go
+++ go_tcp_connection.go
@@ -386,7 +386,7 @@ func (client *Client) On(event string, callback func([]byte)) {
 func (client *Client) Listen() {
 	client.Logger.Log(lgr.Info, "Started listening")
 	for !client.ShouldStop {
-		client.ReceiveData()
+		go client.ReceiveData()
 	}
 }
 
