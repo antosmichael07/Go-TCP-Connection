2572dd311067b5145a91b9955253bf46cfcf1b63
Antoš Michael <antosmichael07@gmail.com>
2024-07-16 14:04:53 +0200

    the if was suppossed to be where it always was

--- go_tcp_connection.go
+++ go_tcp_connection.go
@@ -420,6 +420,10 @@ func (client *Client) ReceiveData() {
 	// Read the data
 	data := make([]byte, 16384)
 	n, err := client.Connection.Read(data)
+	if client.Token != [64]byte{} {
+		// Tell the server that the data was received
+		client.SendData(event_last_data_received, &[]byte{})
+	}
 	data = data[:n]
 	if err != nil {
 		client.Logger.Log(lgr.Error, "Error reading received data: %s", err)
@@ -443,11 +447,6 @@ func (client *Client) ReceiveData() {
 			client.Events[pkg.Event](&pkg.Data)
 			break
 		}
-	}
-
-	if client.Token != [64]byte{} {
-		// Tell the server that the data was received
-		client.SendData(event_last_data_received, &[]byte{})
 	}
 }
 
