e4d55fab3a001126bae9168b7e400a0d5651aec2
Antoš Michael <antosmichael07@gmail.com>
2024-07-16 11:07:35 +0200

    that if broke everything

--- go_tcp_connection.go
+++ go_tcp_connection.go
@@ -420,10 +420,8 @@ func (client *Client) ReceiveData() {
 	// Read the data
 	data := make([]byte, 16384)
 	n, err := client.Connection.Read(data)
-	if client.Token != [64]byte{} {
-		// Tell the server that the data was received
-		client.SendData(event_last_data_received, &[]byte{})
-	}
+	// Tell the server that the data was received
+	client.SendData(event_last_data_received, &[]byte{})
 	data = data[:n]
 	if err != nil {
 		client.Logger.Log(lgr.Error, "Error reading received data: %s", err)
